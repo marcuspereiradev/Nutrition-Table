dca552a4576650917b8ad0b8c8ab81266d5eb0b9
marcuspereiragithub <vinnycoast@gmail.com>
2017-10-05 12:42:20 -0300

    adding new js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,52 +1,68 @@
+// Para selecionar todas as class ou ID's usa-se document.querySelectorAll.
+// Se for uma de cada vez usa-se document.querySelector.
+
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-	// Para selecionar todas as class ou ID's usa-se document.querySelectorAll.
-	// Se for uma de cada vez usa-se document.querySelector.
 var pacientes = document.querySelectorAll(".paciente");
 
+// length para saber o array(ordem, sequência, quantidade).
+for (var i = 0; i < pacientes.length; i++) {
 
-	// length para saber o array(ordem, sequência, quantidade).
-for (i = 0; i < pacientes.length; i++){
-	
-	var paciente = pacientes[i];	
-	
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
+    var paciente = pacientes[i];
 
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-	var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-	var pesoEValido = true;
-	var alturaEValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-	if (peso <= 0 || peso >= 1000) {
-		console.log("Peso inválido!");
-		pesoEValido = false;
-		tdImc.textContent = "Peso Inválido";
-		paciente.classList.add("paciente-invalido");
-	}
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-	if (altura <= 0 || altura >= 3.00) {
-		console.log("Altura Inválida");
-		alturaEValida = false;
-		tdImc.textContent = "Altura Inválida";
-		paciente.classList.add("paciente-invalido");
-	}
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-	if (pesoEValido && alturaEValida) {
-		var imc = calculaImc(peso,altura);
-		tdImc.textContent = imc;
-	}
+    if (!alturaEhValida) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
 }
 
-function calculaImc(peso,altura){
-	var imc = 0
-	imc = peso / (altura * altura);
-	return imc.toFixed(2);
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
 }
 
+function validaPeso(peso) {
 
-	
+    if (peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
--- js/form.js
+++ js/form.js
@@ -1,47 +1,107 @@
-	// Escutador de evento (addEventListener) e (event.preventDefault) que permite segurar um evento padrão do html, ele segura o valor quando o botão é clicado mantendo os valores e não apagando.
+// Escutador de evento (addEventListener) e (event.preventDefault) que permite segurar um evento padrão do html, ele segura o valor quando o botão é clicado mantendo os valores e não apagando.
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-event.preventDefault();
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
 
-	// Pegando os dados do form. form que é a tag, (nome, altura etc) que é o name da tag input e .value que pega o valor digitando nesses input's.	
-var form = document.querySelector("#form-adiciona");
+    var form = document.querySelector("#form-adiciona");
 
-var nome = form.nome.value;
-var peso = form.peso.value;
-var altura = form.altura.value;
-var gordura = form.gordura.value;
+    var paciente = obtemPacienteDoFormulario(form);
 
-	// Criando um tr com CreateElement (cria o elemento que quiser)	
-var pacienteTr = document.createElement("tr");
+    var erros = validaPaciente(paciente);
 
-	// Criando um td com CreateElement	
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
 
-	// Preenchedo as td's com valores dos dados que entram no form	
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calculaImc(peso,altura);
+        return;
+    }
 
+    adicionaPacienteNaTabela(paciente);
 
-	// Colocando os td's dentro do tr usando appendChild
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(imcTd);
+    form.reset();
 
-	
-	// Colocando o tr criado acima dentro do tbody que é a tag onde está tabela. 
-var tabela = document.querySelector("#tabela-pacientes");
-tabela.appendChild(pacienteTr);
-	
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
 });
 
+function obtemPacienteDoFormulario(form) {
 
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
 
+    return paciente;
+}
+
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
